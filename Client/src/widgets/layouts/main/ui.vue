9f6aef8fcf73720e3f8ee65068c4b0a23319eee7
Edanriell <Edanriell@gmail.com>
2024-08-09 01:51:46 +0300

    minor image fixes

--- Client/src/widgets/layouts/main/ui.vue
+++ Client/src/widgets/layouts/main/ui.vue
@@ -11,10 +11,22 @@
 		background: center / cover no-repeat url("/images/raster/background-mobile-1x.jpg");
 	}
 
+	@supports (background-image: url("data:image/webp;")) {
+		.main {
+			background: center / cover no-repeat url("/images/raster/background-mobile-1x.webp");
+		}
+	}
+
 	@media (-webkit-min-device-pixel-ratio: 2),
 	(min-resolution: 192dpi) {
 		.main {
 			background: center / cover no-repeat url("/images/raster/background-mobile-2x.jpg");
+		}
+
+		@supports (background-image: url("data:image/webp;")) {
+			.main {
+				background: center / cover no-repeat url("/images/raster/background-mobile-2x.webp");
+			}
 		}
 	}
 
@@ -22,12 +34,24 @@
 		.main {
 			background: center / cover no-repeat url("/images/raster/background-desktop-1x.jpg");
 		}
+
+		@supports (background-image: url("data:image/webp;")) {
+			.main {
+				background: center / cover no-repeat url("/images/raster/background-desktop-1x.webp");
+			}
+		}
 	}
 
 	@media (min-width: 768px) and (-webkit-min-device-pixel-ratio: 2),
 	(min-width: 768px) and (min-resolution: 192dpi) {
 		.main {
 			background: center / cover no-repeat url("/images/raster/background-desktop-2x.jpg");
+		}
+
+		@supports (background-image: url("data:image/webp;")) {
+			.main {
+				background: center / cover no-repeat url("/images/raster/background-desktop-2x.webp");
+			}
 		}
 	}
 </style>
