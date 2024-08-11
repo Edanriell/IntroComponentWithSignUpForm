9fce943be052890ecf2014a5ad306c7bc7f87fa2
Edanriell <Edanriell@gmail.com>
2024-08-11 13:38:20 +0300

    finished input

---
+++ Client/src/shared/ui/input/index.ts
@@ -0,0 +1,3 @@
+import { default as Input } from "./ui.vue";
+
+export { Input };
---
+++ Client/src/shared/ui/input/ui.vue
@@ -0,0 +1,50 @@
+<script lang="ts" setup>
+	type Props = {
+		classes?: string;
+		name: string;
+		description: string;
+		placeholder: string;
+		type?: "text" | "password" | "email";
+	};
+
+	const props = withDefaults(defineProps<Props>(), {
+		classes: "",
+		type: "text"
+	});
+</script>
+
+<template>
+	<div :class="props.classes">
+		<label :for="props.name" class="visually-hidden">{{ props.description }}</label>
+		<input
+			:id="props.name"
+			:name="props.name"
+			:placeholder="props.placeholder"
+			:type="props.type"
+			class="input"
+		/>
+	</div>
+</template>
+
+<style scoped>
+	.input {
+		border: 0.1rem solid var(--kingly-cloud);
+		border-radius: 0.5rem;
+		width: 100%;
+		padding: 1.5rem 4.9rem 1.5rem 1.941rem;
+		font-family: var(--font-family), sans-serif;
+		font-weight: 600;
+		font-size: 1.4rem;
+		line-height: 186%;
+		letter-spacing: 0.02em;
+		color: var(--martinique);
+
+		&::placeholder {
+			opacity: 0.75;
+		}
+
+		@media (width >= 1440px) {
+			padding: 1.5rem 4.9rem 1.5rem 3.2rem;
+		}
+	}
+</style>
