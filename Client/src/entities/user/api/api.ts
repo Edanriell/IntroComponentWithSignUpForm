e3d67d0a3a112b02638ad5ae7c14d5b2f575c1a9
Edanriell <Edanriell@gmail.com>
2024-08-14 13:47:51 +0300

    finished client api

---
+++ Client/src/entities/user/api/api.ts
@@ -0,0 +1,42 @@
+import { postData } from "@shared/api";
+
+type CreateUserParameters = {
+	firstName: string;
+	lastName: string;
+	email: string;
+	password: string;
+};
+
+export const createUser = async ({
+	firstName,
+	lastName,
+	email,
+	password
+}: CreateUserParameters) => {
+	try {
+		const response = await postData("endpoint", {
+			FirstName: firstName,
+			LastName: lastName,
+			Email: email,
+			Password: password
+		});
+
+		if (!response.ok) {
+			const errorText = await response.text();
+			throw new Error(
+				`Failed to send user data. Status: ${response.status}. Message: ${errorText || "Unknown error"}`
+			);
+		}
+
+		const contentType = response.headers.get("Content-Type");
+
+		if (!contentType || !contentType.includes("application/json")) {
+			throw new Error("Expected JSON response but received a different format.");
+		}
+
+		return await response.json();
+	} catch (error) {
+		console.error("An error occurred while sending the user data:", error);
+		throw error;
+	}
+};
